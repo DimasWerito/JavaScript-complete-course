8f276ccd9b3f65daf36b78df857aea5c5a5b2404
Dmytro Krasilnykov <krasilnykov@gmail.com>
2024-09-12 13:49:51 +0200

    variables

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,32 @@
-let js = "amazing";
+/*
+let country = "Ukraine";
+let continent = "Europe";
+let population = "40 million";
 
-console.log(40 + 8 + 23 - 10);
+// console.log(country);
+console.log(continent);
+console.log(population);
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof "Jonas");
+
+javascriptIsFun = "YES!";
+console.log(javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
+*/
+
+let age = 30;
+age = 31;

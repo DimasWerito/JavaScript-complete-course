f113c9045bb3ccb6dde1c957fb55e87a251903c4
Dmytro Krasilnykov <krasilnykov@gmail.com>
2024-09-13 11:37:36 +0200

    ternary operator

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -190,7 +190,6 @@ if (hasDriversLicense && hasGoodVision && !isTired) {
 } else {
   console.log("Someone else should drive...");
 }
-*/
 
 const day = "friday";
 
@@ -231,3 +230,21 @@ if (day === "monday") {
 } else {
   console.log("Not a valid day!");
 }
+*/
+
+const age = 17;
+age >= 18
+  ? console.log("I like to drink wine 🍷")
+  : console.log("I like to drink water 💧");
+
+const drink = age >= 18 ? "wine 🍷" : "water 💧";
+console.log(drink);
+
+let drink2;
+if (age >= 18) {
+  drink2 = "wine 🍷";
+} else {
+  drink2 = "water 💧";
+}
+console.log(drink2);
+console.log(`I like to drink ${age >= 18 ? "wine 🍷" : "water 💧"}`);
